7949e8a8aa775002106c591df29ff2bab3431149
Sachin Verma <vsachin094@gmail.com>
2023-09-20 12:24:53 +0530

    interface parse added

--- jinja_yaml_render/interface_parse.py
+++ jinja_yaml_render/interface_parse.py
@@ -1,50 +1,39 @@
 import re
 
-# Sample 'show interfaces' output
-show_interfaces_output = """
-Interface GigabitEthernet1/0/1
+# Sample 'show interfaces bundle-ether *' output
+show_interfaces_bundle_ether_output = """
+Bundle-Ether1 line protocol is admin down
     Description: Connection to Server
     Status: Up
-    Speed: 1000 Mbps
+    Speed: 10000 Mbps
 
-Interface GigabitEthernet1/0/2
+Bundle-Ether2 line protocol is admin up
     Description: Connection to Switch
     Status: Up
-    Speed: 1000 Mbps
+    Speed: 10000 Mbps
 
-Interface GigabitEthernet1/0/3
+Bundle-Ether3 line protocol is up
     Description: Connection to Printer
     Status: Down
     Speed: 1000 Mbps
 """
 
-# Define a regex pattern to match the start of each interface section
-interface_pattern = r'Interface (\S+)'
+# Define a regex pattern to match the "Bundle-Ether" interfaces and their lines
+bundle_ether_pattern = r'Bundle-Ether\S+.*?(?=\nBundle-Ether|\Z)'
 
-# Use the findall method to find all interface sections
-interface_sections = re.findall(interface_pattern, show_interfaces_output)
+# Use re.findall to extract each "Bundle-Ether" section
+bundle_ether_sections = re.findall(bundle_ether_pattern, show_interfaces_bundle_ether_output, re.DOTALL)
 
-# Initialize a list to store the lines for each interface
-interface_lines_list = []
+# Initialize a list to store the raw lines for each "Bundle-Ether" interface
+bundle_ether_lines_list = []
 
-# Iterate through the interface sections and store the lines in the list
-for interface_name in interface_sections:
-    # Create a regex pattern to match the section from start to end
-    section_pattern = rf'Interface {re.escape(interface_name)}(.*?)\n\n|Interface {re.escape(interface_name)}(.*)$'
-    
-    # Use re.search to find the section in the output
-    match = re.search(section_pattern, show_interfaces_output, re.DOTALL)
-    
-    if match:
-        # Extract and store the matched section (including the start pattern)
-        section = match.group(0)
-        
-        # Split the section into lines and store them in the list
-        lines = section.strip().split('\n')
-        interface_lines_list.append(lines)
+# Iterate through the "Bundle-Ether" sections and store the raw lines in the list
+for section in bundle_ether_sections:
+    lines = section.strip().split('\n')
+    bundle_ether_lines_list.append(lines)
 
-# Now you have a list of lists where each inner list contains the lines for an interface
-# For example, to access the lines for GigabitEthernet1/0/1:
-# gigabit_eth1_0_1_lines = interface_lines_list[0]
-print(interface_lines_list)
-# You can do the same for other interfaces as well
+# Now you have a list of lists where each inner list contains the raw lines for a "Bundle-Ether" interface
+# For example, to access the raw lines for Bundle-Ether1:
+bundle_eth1_lines = bundle_ether_lines_list[0]
+print(bundle_eth1_lines)
+# You can do the same for other "Bundle-Ether" interfaces as well
