9199653a71b29bcad069807159a4b6d9305f171f
Sachin Verma <vsachin094@gmail.com>
2024-05-10 16:05:46 +0530

    Create NSO_Orchestration.py

---
+++ NSO_Orchestration.py
@@ -0,0 +1,16 @@
+Solution:
+Presenting our groundbreaking automated onboarding solution for Cisco NSO and Crosswork. With our innovative system, device onboarding time is dramatically reduced from 15-20 minutes to an impressive 1-2 minutes, ensuring swift and precise setups.
+
+Moreover, our automation platform offers advanced functionality by supporting auto deboarding. This feature ensures that devices no longer active within the network are automatically removed, optimizing system cleanliness and efficiency.
+
+What sets our solution apart is its utilization of NMDB as a primary data source. Leveraging NMDB, our system intelligently identifies devices associated with NMDB groups, streamlining the onboarding process. Furthermore, all these operations are seamlessly orchestrated through scheduled jobs, providing a hands-off and efficient approach to device management.
+
+Bid farewell to manual complexities and welcome a future where device onboarding is seamless and efficient. Our automated solution stands ready to revolutionize your workflow and elevate operational efficiency to new heights.
+
+
+
+Within our engineering operations team, the manual onboarding process for devices into Cisco NSO (Network Services Orchestrator) and Crosswork poses significant time and error challenges. Currently, each device's manual onboarding can consume between 15 to 20 minutes, a timeframe that not only hampers productivity but also increases the likelihood of errors due to the multitude of steps involved in the process.
+
+This inefficiency is particularly pronounced in environments where multiple devices need to be onboarded swiftly to meet network demands or deploy new services. The time-intensive nature of manual onboarding not only strains resources but also inhibits our team's agility in responding to dynamic network requirements.
+
+Moreover, the risk of human error looms large during manual onboarding, potentially leading to configuration inconsistencies, service disruptions, or security vulnerabilities. These challenges underscore the urgent need for an automated solution that not only accelerates the onboarding process but also enhances accuracy, reliability, and scalability.
